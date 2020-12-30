dde46d0ecd4e3816d655a80cf665ba8c2353f87f
Crowdbotics <team@crowdbotics.com>
2020-12-30 11:44:04 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/konnect_by_hbl_23608/MainActivity.java
@@ -0,0 +1,15 @@
+package com.konnect_by_hbl_23608;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "konnect_by_hbl_23608";
+    }
+}
